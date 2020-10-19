11df14fe08f2aee24bb2954407d2893473a1b570
Keith Holler <robert.keith.holler@gmail.com>
2020-10-19 08:58:19 -0400

    Icons,Favorites, and Comments

---
+++ components/ContactComponent.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { ScrollView } from "react-native";
+import { Text, Card } from "react-native-elements";
+
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  static navigationOptions = {
+    title: "Contact Us",
+  };
+
+  render() {
+
+   
+    return (
+      <ScrollView>
+        <Card wrapperStyle={{ margin: 20 }} title="Contact Information">
+          <Text>
+            1 Nucamp Way{"\n"}
+            Seattle, WA 98001{"\n"}
+            U.S.A.{"\n"}
+            {"\n"}
+            Phone: 1-206-555-1234{"\n"}
+            Email:campsites@nucamp.co
+          </Text>
+        </Card>
+      </ScrollView>
+    );
+  }
+}
+
+export default Contact;
